0a03fb579a360ca3e8a321b92537cbff3b5e94c0
kevinresol <kevinresol@gmail.com>
2016-06-17 12:12:09 +0800

    Compile fix

--- src/travix/loggers/JsLogger.hx
+++ src/travix/loggers/JsLogger.hx
@@ -2,23 +2,23 @@ package travix.loggers;
 
 class JsLogger {
   
-  static var callPhantom = untyped js.Browser.window.callPhantom;
+  static var callPhantom:Dynamic = untyped js.Browser.window.callPhantom;
   
-  static function print(s:String) {
+  public static function print(s:String) {
     callPhantom({
      cmd: 'travix:print',
      message: s,
    });
   }
   
-  static function println(s:String) {
+  public static function println(s:String) {
     callPhantom({
      cmd: 'travix:println',
      message: s,
    });
   }
   
-  static function exit(code:Int) {
+  public static function exit(code:Int) {
     callPhantom({
      cmd: 'travix:exit',
      exitCode: 0,
