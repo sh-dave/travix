e2c31186a593736c9c060845afaf19623b27a801
Andreas <ciscoheat@gmail.com>
2017-02-02 16:12:03 +0100

    Lua is working now with buddy 2.6.2 or later.

--- src/travix/commands/LuaCommand.hx
+++ src/travix/commands/LuaCommand.hx
@@ -1,7 +1,50 @@
 package travix.commands;
 
+import Sys.*;
+
 class LuaCommand extends Command {
   override function execute() {
-    
+    if(command('eval', ['which luarocks >/dev/null']) != 0) {
+      foldOutput('lua-install', function() {
+        // Required repo for cmake (for luv library)
+        exec('eval', ['sudo add-apt-repository ppa:george-edison55/precise-backports']);
+        exec('eval', ['sudo apt-get update']);
+
+        for(pack in ["lua5.2","make","cmake","unzip","libpcre3","libpcre3-dev"]) 
+          aptGet(pack);
+
+        // Add source files so luarocks can be compiled
+        exec('sudo', ['mkdir', '-p', '/usr/include/lua/5.2']);
+        exec('wget', ['-q', 'http://www.lua.org/ftp/lua-5.2.0.tar.gz']);
+        exec('tar', ['xf', 'lua-5.2.0.tar.gz']);
+        exec('eval', ['sudo cp lua-5.2.0/src/* /usr/include/lua/5.2']);
+        exec('rm', ['-rf', 'lua-5.2.0']);
+        exec('rm', ['-f', 'lua-5.2.0.tar.gz']);
+
+        // Compile luarocks
+        exec('wget', ['-q', 'http://luarocks.org/releases/luarocks-2.3.0.tar.gz']);
+        exec('tar', ['zxpf', 'luarocks-2.3.0.tar.gz']);
+
+        withCwd('luarocks-2.3.0', function() {
+          exec('./configure');
+          exec('eval', ['make build >/dev/null']);
+          exec('eval', ['sudo make install >/dev/null']);
+        });
+
+        exec('rm', ['-f', 'luarocks-2.3.0.tar.gz']);
+        exec('rm', ['-rf', 'luarocks-2.3.0']);
+
+        // Install lua libraries
+        // Based on https://github.com/HaxeFoundation/haxe/blob/3a6d024019aad28ab138fbb88cade34ff2e5bf19/tests/RunCi.hx#L473
+        exec('eval', ['sudo luarocks install lrexlib-pcre 2.8.0-1']);
+        exec('eval', ['sudo luarocks install luv 1.9.1-0']);
+        exec('eval', ['sudo luarocks install luasocket 3.0rc1-2']);
+        exec('eval', ['sudo luarocks install environ 0.1.0-1']);
+      });
+    }
+
+    build(['-lua', 'bin/lua/tests.lua'], function () {
+      exec('lua', ['bin/lua/tests.lua']);
+    });
   }
 }
