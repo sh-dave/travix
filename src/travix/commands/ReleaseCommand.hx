70c8e1288e55ccdc6be1da3e81a7da8da76a94f7
Kevin Leung <kevinresol@gmail.com>
2017-03-16 11:34:35 +0800

    Fix

--- src/travix/commands/ReleaseCommand.hx
+++ src/travix/commands/ReleaseCommand.hx
@@ -1,13 +1,13 @@
 package travix.commands;
 
-import Sys.*;
+import sys.io.*;
 
 class ReleaseCommand extends Command {
   
   override function execute() {
-    if(command('haxelib', ['path', 'travix_release'] != 0) {
+    if(new Process('haxelib', ['path', 'travix_release']).exitCode() != 0) {
       exec('haxelib', ['install', 'travix_release']);
     }
-    exec('haxelib', ['run', 'travix_release']);
+    exec('haxelib', ['run', 'travix_release'].concat(this.args));
   }
 }
