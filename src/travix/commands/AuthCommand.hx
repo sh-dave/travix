9b0af35973a6e278e2c17d86bbef629a0ca15745
Kevin Leung <kevinresol@gmail.com>
2017-03-16 11:58:36 +0800

    Use installLib

--- src/travix/commands/AuthCommand.hx
+++ src/travix/commands/AuthCommand.hx
@@ -6,7 +6,7 @@ class AuthCommand extends Command {
   
   override function execute() {
     if(!libInstalled('travix_auth')) {
-      exec('haxelib', ['install', 'travix_auth']);
+      installLib('travix_auth');
     }
     exec('haxelib', ['run', 'travix_auth'].concat(this.args));
   }
--- src/travix/commands/ReleaseCommand.hx
+++ src/travix/commands/ReleaseCommand.hx
@@ -6,7 +6,7 @@ class ReleaseCommand extends Command {
   
   override function execute() {
     if(!libInstalled('travix_release')) {
-      exec('haxelib', ['install', 'travix_release']);
+      installLib('travix_release');
     }
     exec('haxelib', ['run', 'travix_release'].concat(this.args));
   }
