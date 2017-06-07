35e58ce22731a5da2a25badfcdd4b559b03789a4
Kevin Leung <kevinresol@gmail.com>
2017-06-07 11:34:24 +0800

    Publish to npm

--- src/travix/Macro.hx
+++ src/travix/Macro.hx
@@ -6,10 +6,18 @@ using haxe.io.Path;
 #if macro
 import haxe.macro.MacroStringTools;
 import haxe.macro.Context;
+import haxe.macro.Compiler;
 #end
 
 class Macro {
 	#if macro
+	public static function shebang() {
+		Context.onAfterGenerate(function() {
+			var out = Compiler.getOutput();
+			File.saveContent(out, '#!/usr/bin/env node\n\n' + File.getContent(out));
+		});
+	}
+	
 	public static function loadFile(name:String) {
 		return MacroStringTools.formatString(File.getContent(Context.getPosInfos((macro null).pos).file.directory() + '/$name'), Context.currentPos());
 	}
