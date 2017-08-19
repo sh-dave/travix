34f8ee95cfcb7f28bd1bdb52ab34f664a78067a1
Sebastian Thomschke <sebthom@users.noreply.github.com>
2017-08-19 17:33:36 +0200

    Correct source url for Ubuntu precise

--- src/travix/commands/CsCommand.hx
+++ src/travix/commands/CsCommand.hx
@@ -14,7 +14,7 @@ class CsCommand extends Command {
       exec('eval', ['sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF']);
       switch tryToRun('lsb_release', ['-cs']) {
         case Success('precise\n'):
-          exec('eval', ['echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/mono-official.list']);
+          exec('eval', ['echo "deb http://download.mono-project.com/repo/ubuntu precise main" | sudo tee /etc/apt/sources.list.d/mono-official.list']);
         case Success('trusty\n'):
           exec('eval', ['echo "deb http://download.mono-project.com/repo/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/mono-official.list']);
         case Success('xenial\n'):
