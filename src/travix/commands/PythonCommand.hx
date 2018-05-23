fdc4a83278e52c9f2c17a021304b76ada0a274a8
Kevin Leung <kevinresol@gmail.com>
2018-05-23 22:50:24 +0800

    Move python install script

--- src/travix/commands/PythonCommand.hx
+++ src/travix/commands/PythonCommand.hx
@@ -5,21 +5,20 @@ import tink.cli.Rest;
 class PythonCommand extends Command {
 
   public function install() {
-    
+    if (tryToRun('python3', ['--version']).match(Failure(_, _))) {
+
+      // fix for https://github.com/back2dos/travix/issues/83
+      if (Sys.systemName() == 'Mac' && tryToRun('python', ['--version']).match(Success(_))) {
+        // https://stackoverflow.com/questions/49672642/trying-to-install-python3-using-brew
+        exec('brew', ['upgrade', "python"]);
+      } else {
+        installPackage('python3');
+      }
+    }
   }
 
   public function buildAndRun(rest:Rest<String>) {
     build('python', ['-python', 'bin/python/tests.py'].concat(rest), function () {
-      if (tryToRun('python3', ['--version']).match(Failure(_, _))) {
-
-        // fix for https://github.com/back2dos/travix/issues/83
-        if (Sys.systemName() == 'Mac' && tryToRun('python', ['--version']).match(Success(_))) {
-          // https://stackoverflow.com/questions/49672642/trying-to-install-python3-using-brew
-          exec('brew', ['upgrade', "python"]);
-        } else {
-          installPackage('python3');
-        }
-      }
       exec('python3', ['bin/python/tests.py']);
     });
   }
