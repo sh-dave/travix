f755a9143e10744eb17d2256261daa4ad7401337
Juraj Kirchheim <back2dos@googlemail.com>
2018-08-13 18:15:36 +0200

    Update JsLogger.hx

--- src/travix/loggers/JsLogger.hx
+++ src/travix/loggers/JsLogger.hx
@@ -5,24 +5,30 @@ class JsLogger {
   static var callPhantom:Dynamic = untyped js.Browser.window.callPhantom;
   
   public static function print(s:String) {
-    callPhantom({
-     cmd: 'travix:print',
-     message: s,
-   });
+    if (callPhantom)
+      callPhantom({
+        cmd: 'travix:print',
+        message: s,
+      });
+    else console.log(s);
   }
   
   public static function println(s:String) {
-    callPhantom({
-     cmd: 'travix:println',
-     message: s,
-   });
+    if (callPhantom)
+      callPhantom({
+        cmd: 'travix:println',
+        message: s,
+      });
+    else console.log(s);
   }
   
   public static function exit(code:Int) {
-    callPhantom({
-     cmd: 'travix:exit',
-     exitCode: code,
-   });
+    if (callPhantom)
+      callPhantom({
+        cmd: 'travix:exit',
+        exitCode: code,
+      });
+    else if (code != 0) throw code;//certainly not ideal, but it should work ^^
   }
   
 }
