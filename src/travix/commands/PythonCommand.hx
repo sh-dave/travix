1fc53b2678856506294d82d60713b8e0623fadf9
Kevin Leung <kevinresol@gmail.com>
2018-10-18 01:03:27 +0800

    try

--- src/travix/commands/PythonCommand.hx
+++ src/travix/commands/PythonCommand.hx
@@ -14,8 +14,8 @@ class PythonCommand extends Command {
       } else {
         installPackage('python3');
         if(Travix.isWindows) {
-          exec('python', ['--version']);
           exec('python3', ['--version']);
+          exec('python', ['--version']);
         }
       }
     }
